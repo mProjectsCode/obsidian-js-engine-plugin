0cb65a71b399501aaf7b7c76ec580527def6bb76
Jan Wille <mail@janwille.de>
2025-02-26 15:39:35 +0100

    add setting `startupScriptsDirectory`

--- jsEngine/settings/Settings.ts
+++ jsEngine/settings/Settings.ts
@@ -1,12 +1,14 @@
 import type JsEnginePlugin from 'jsEngine/main';
 import type { App } from 'obsidian';
-import { PluginSettingTab, Setting } from 'obsidian';
+import { normalizePath, PluginSettingTab, Setting } from 'obsidian';
 
 export interface JsEnginePluginSettings {
-	startupScripts?: string[];
+	startupScriptsDirectory: string | undefined;
+	startupScripts: string[];
 }
 
 export const JS_ENGINE_DEFAULT_SETTINGS: JsEnginePluginSettings = {
+	startupScriptsDirectory: undefined,
 	startupScripts: [],
 };
 
@@ -26,5 +28,35 @@ export class JsEnginePluginSettingTab extends PluginSettingTab {
 		if (!settings) {
 			return;
 		}
+
+		new Setting(containerEl)
+			.setName('JS snippets (loaded on startup)')
+			.setHeading()
+			.addExtraButton(el => {
+				el.setTooltip('Open snippets folder')
+					.setIcon('folder-open')
+					.onClick(async () => await this.openStartupScriptsDirectory());
+			});
+
+		new Setting(containerEl)
+			.setName('Custom JS Snippets Folder')
+			.setDesc('The folder to search for JavaScript files to load')
+			.addText(el => {
+				el.setPlaceholder('Folder')
+					.setValue(settings.startupScriptsDirectory ?? '')
+					.onChange(async val => {
+						settings.startupScriptsDirectory = val ? normalizePath(val) : undefined;
+						await this.plugin.saveSettings();
+					});
+			});
+	}
+
+	async openStartupScriptsDirectory(): Promise<void> {
+		const vault = this.app.vault;
+		const directory = this.plugin.settings.startupScriptsDirectory ?? '/';
+		if (!(await vault.adapter.exists(directory))) {
+			await vault.createFolder(directory);
+		}
+		this.app.openWithDefaultApp(directory);
 	}
 }
