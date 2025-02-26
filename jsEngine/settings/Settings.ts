862d1533b63ce758cd074fd213b174bd1c4e82ae
Jan Wille <mail@janwille.de>
2025-02-26 16:03:08 +0100

    startupScript settings inspired by css-snippets

--- jsEngine/settings/Settings.ts
+++ jsEngine/settings/Settings.ts
@@ -1,6 +1,6 @@
 import type JsEnginePlugin from 'jsEngine/main';
 import type { App } from 'obsidian';
-import { normalizePath, PluginSettingTab, Setting } from 'obsidian';
+import { normalizePath, PluginSettingTab, Setting, TFile, TFolder } from 'obsidian';
 
 export interface JsEnginePluginSettings {
 	startupScriptsDirectory: string | undefined;
@@ -33,6 +33,11 @@ export class JsEnginePluginSettingTab extends PluginSettingTab {
 			.setName('JS snippets (loaded on startup)')
 			.setHeading()
 			.addExtraButton(el => {
+				el.setTooltip('Reload snippets')
+					.setIcon('refresh-cw')
+					.onClick(() => this.display());
+			})
+			.addExtraButton(el => {
 				el.setTooltip('Open snippets folder')
 					.setIcon('folder-open')
 					.onClick(async () => await this.openStartupScriptsDirectory());
@@ -49,6 +54,23 @@ export class JsEnginePluginSettingTab extends PluginSettingTab {
 						await this.plugin.saveSettings();
 					});
 			});
+
+		const startupScriptsDirectory = this.app.vault.getFolderByPath(settings.startupScriptsDirectory ?? '/');
+		let startupScripts: TFile[] = [];
+		if (startupScriptsDirectory != null) {
+			startupScripts = this.listJSfilesInDirectory(startupScriptsDirectory);
+		}
+		if (startupScripts.length == 0) {
+			new Setting(containerEl).setName('No JS snippets found').setDesc(`JS snippets are stored in "vault/${settings.startupScriptsDirectory ?? ''}"`);
+		}
+		for (const file of startupScripts) {
+			new Setting(containerEl)
+				.setName(file.basename)
+				.setDesc(`Apply JS snippet from "vault/${file.path}"`)
+				.addToggle(el => {
+					el.setValue(settings.startupScripts.contains(file.path)).onChange(async val => this.toggleStartupScript(file, val));
+				});
+		}
 	}
 
 	async openStartupScriptsDirectory(): Promise<void> {
@@ -58,5 +80,21 @@ export class JsEnginePluginSettingTab extends PluginSettingTab {
 			await vault.createFolder(directory);
 		}
 		this.app.openWithDefaultApp(directory);
+	}
+
+	listJSfilesInDirectory(directory: TFolder): TFile[] {
+		const files = directory.children.filter(el => el instanceof TFile);
+		const folders = directory.children.filter(el => el instanceof TFolder);
+		return files.filter(f => f.extension == 'js').concat(folders.flatMap(dir => this.listJSfilesInDirectory(dir)));
+	}
+
+	async toggleStartupScript(file: TFile, enable: boolean): Promise<void> {
+		const settings = this.plugin.settings;
+		if (enable) {
+			settings.startupScripts.push(file.path);
+		} else {
+			settings.startupScripts.remove(file.path);
+		}
+		await this.plugin.saveSettings();
 	}
 }
